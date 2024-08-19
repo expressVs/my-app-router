d516c7b4b0f023c923db65e549fd866969faefe8
OceanPowerY <1250123861@qq.com>
2024-08-19 21:46:57 +0800

    browserRouter的基础使用+basename属性

--- src/index.js
+++ src/index.js
@@ -2,16 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function Bpp(){
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  )
+}
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // browserRouter的属性basename可以设置根路径， 多个项目，进行路由合并的时候使用
+  <BrowserRouter basename='pc'>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/bpp" element={<Bpp />} />
+    </Routes>
+  </BrowserRouter>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
