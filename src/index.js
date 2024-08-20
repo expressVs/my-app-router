c167fccbc841e40cbe1289008aa644631648b5b4
OceanPowerY <1250123861@qq.com>
2024-08-20 19:51:48 +0800

    outlet实现通用Layout（outlet实际上是子页面的占位符）

--- src/index.js
+++ src/index.js
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { HashRouter, BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './layout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function Bpp (){
+  return (
+    <div>
+      <h1> BPP </h1>
+    </div>
+  )
+}
+
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <Routes>
-      <Route path="/app" element={<App />} />
+      <Route path="/" element={<Layout />} >
+        <Route path="/app" element={<App />} />
+        <Route path="/bpp" element={<Bpp />} />
+      </Route>
     </Routes>
-  </HashRouter>
+  </BrowserRouter>
 
 );
